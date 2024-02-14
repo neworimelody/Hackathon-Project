cf0f3ff4023be0521e7f413b64b8d7d0b7e71738
neworimelody <146850779+neworimelody@users.noreply.github.com>
2024-02-14 11:27:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-console.log ("hello")
+url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Language%20%26%20Literature/Most%20Spoken%20Languages%20Worldwide.csv"
+var language = (column )
