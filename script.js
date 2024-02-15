f216990dca6f6310f3bf7191bead7d712ebb0cc8
neworimelody <146850779+neworimelody@users.noreply.github.com>
2024-02-15 13:29:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Language%20%26%20Literature/Most%20Spoken%20Languages%20Worldwide.csv"
-var language = (column )
+var language = getColumn (url, 2);
+var branch = getColumn (url, 6);
+var rank = getColumn (url, 1);
+
+
